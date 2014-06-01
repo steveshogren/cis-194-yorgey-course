bc727f2a49a802071b41a6949dac5f3ea7f8e4c5
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-01 15:10:32 -0400

    A function that doubles every other number, starting from the back

--- chap01/exercise1.hs
+++ chap01/exercise1.hs
@@ -5,3 +5,11 @@ toDigits t
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherBackwards . reverse
+
+doubleEveryOtherBackwards :: [Integer] -> [Integer]
+doubleEveryOtherBackwards [] = []
+doubleEveryOtherBackwards (h:[]) = [(*) h 2]
+doubleEveryOtherBackwards (h:(n:ts)) = (*) h 2 : n : doubleEveryOtherBackwards ts
