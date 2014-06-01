ba94c0bc0b46374cc44385d00f002d99c5224166
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-01 15:52:23 -0400

    A towers of hanoi solver using recursion

--- chap01/exercise1.hs
+++ chap01/exercise1.hs
@@ -17,7 +17,21 @@ doubleEveryOtherBackwards (h:(n:ts)) = h : (* 2) n : doubleEveryOtherBackwards t
 sumDigits :: [Integer] -> Integer
 sumDigits = sum . concat . (map toDigits) 
 
+-- validate 4012888888881881 = True
+-- validate 4012888888881882 = False
 validate :: Integer -> Bool
 validate card = 
    let sum = (sumDigits . doubleEveryOther . toDigits) card
    in sum `mod` 10 == 0
+
+type Peg = String
+type Move = (Peg, Peg)
+-- hanoi 2 "a" "b" "c" == [("a","c"),("a","b"),("c","b")]
+-- hanoi 3 "a" "b" "c" == [("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b")]
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 from to hold = [(from, to)] 
+hanoi n a b c = 
+   hanoi (n-1) a c b
+     ++ hanoi 1 a b c
+     ++ hanoi (n-1) c b a
+
