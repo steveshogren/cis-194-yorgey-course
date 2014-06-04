6625ae0e810b86acd8b916826b0fdf7020e08754
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-04 17:28:06 -0400

    A code golf version of localMaxima

--- chap3/golf.hs
+++ chap3/golf.hs
@@ -1,2 +1,19 @@
+-- skips "test" 
+-- > ["test","est","st","t"]
 skips :: [a] -> [[a]]
 skips a = map (\s -> drop s a) [0..(length a)-1] 
+
+
+--localMaxima [2,9,5,6,1] == [9,6]
+localMaxima :: [Integer] -> [Integer]
+localMaxima = map (head . drop 1) . filter filterMax . groupPart 3
+
+filterMax :: [Integer] -> Bool
+filterMax (a:b:c:_) = b > a && b > c
+filterMax _ = False
+
+groupPart :: Int -> [a] -> [[a]]
+groupPart c l = map (\s -> (take c . drop s) l) [0..(length l)-c]
+
+
+
