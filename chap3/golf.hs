48a18007bfb7eff3b24f82bd6b98cfc5c6e41c23
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-06 06:52:37 -0400

    Finishing the histogram problem

--- chap3/golf.hs
+++ chap3/golf.hs
@@ -20,18 +20,22 @@ groupPart :: Int -> [a] -> [[a]]
 groupPart c l = map (\s -> (take c . drop s) l) [0..(length l)-c]
 
 
-histogram :: [Integer] -> String
+--histogram [1,2,2,4,6,2,2,8,1,1,1]
+--"**       \n**       \n**       \n** * * * \n=========\n123456789"
+histogram :: [Int] -> String
 histogram i =  
    let grp = (groupBy (==) . sort) i
-   in (concat . reverse) $
-      ["123456789","=========\n"] 
-      ++ map (\row -> (map (\s -> if getCount s grp >= row then "*" else "")) [1..9]) [0..getMax grp]
+   in (concat . reverse) $ (["123456789","=========\n"] ++ map (makeRow grp) [1..getMax grp])
 
-      -- ++ (concat . map (\s -> show $ getCount s grp)) [1..9]
-getMax :: [[Integer]] -> Int
-getMax = head . head . reverse . sortBy (compare `on` length) 
+makeRow :: [[Int]] -> Int -> String
+makeRow grp row = (foldl (\accum s -> if getCount s grp >= row 
+                                     then accum++"*" 
+                                     else accum++" ") "" [1..9]) ++ "\n"
 
-getCount :: Integer -> [[Integer]] -> Int
+getMax :: [[Int]] -> Int
+getMax = length . head . reverse . sortBy (compare `on` length) 
+
+getCount :: Int -> [[Int]] -> Int
 getCount n grp =  
     let set = filter (\s -> head s == n)  grp
     in if null set then 0 else (length . head) set
