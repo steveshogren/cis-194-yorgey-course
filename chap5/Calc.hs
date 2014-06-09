9db1306edc54bb76614d0166d67376e434dfa16c
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-09 17:40:21 -0400

    Working instances for the Expr typeclass for a few different "worlds" of data

--- chap5/Calc.hs
+++ chap5/Calc.hs
@@ -23,9 +23,37 @@ class Expr a where
 
 
 instance Expr ExprT where
-         lit = Lit
-         add = Add
-         mul = Mul
+  lit = Lit
+  add = Add
+  mul = Mul
 
 reify :: ExprT -> ExprT
 reify = id
+
+instance Expr Integer where
+  lit = id
+  add = (+) 
+  mul = (*)
+
+instance Expr Bool where
+  lit a = if a > 0 then True else False
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7
+  add a b = mod (a+b) 7
+  mul a b = mod (a*b) 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
