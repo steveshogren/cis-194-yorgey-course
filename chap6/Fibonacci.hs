9ec4a97a5cffeb28caa5b8157b51d86ee75506ef
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-12 17:11:52 -0400

    Making a custom Show for my Stream type, and a streamToList

--- chap6/Fibonacci.hs
+++ chap6/Fibonacci.hs
@@ -11,3 +11,14 @@ fibs2 = 0 : 1 : next fibs2
   where
     next (a : t@(b:_)) = (a+b) : next t
 
+
+data Stream a = Cons a (Stream a)
+
+instance (Show a) => Show(Stream a) where 
+ show (Cons a b) = (concat . map show . take 10 . streamToList) b
+
+streamToList :: Stream a -> [a]
+streamToList (Cons f r) = f:(streamToList r)
+
+
+
