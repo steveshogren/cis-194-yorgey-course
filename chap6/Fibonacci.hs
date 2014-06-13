6db6e4c1a6a2ee283beac0321e56b57925b62810
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-13 07:00:42 -0400

    A streamFromSeed that allows a new stream to be created from a seed value and fn

--- chap6/Fibonacci.hs
+++ chap6/Fibonacci.hs
@@ -14,14 +14,24 @@ fibs2 = 0 : 1 : next fibs2
 
 data Stream a = Cons a (Stream a)
 
+-- this prevents the default "show" from looping infinitely
 instance (Show a) => Show(Stream a) where 
- show (Cons a b) = (concat . map show . take 10 . streamToList) b
+ show (Cons a b) = show a ++ (concat . map show . take 10 . streamToList) b
 
 streamToList :: Stream a -> [a]
 streamToList (Cons f r) = f:(streamToList r)
 
+-- streamRepeat 3 == 3333333333
 streamRepeat :: a -> Stream a
 streamRepeat a = Cons a $ streamRepeat a
 
+-- streamMap (+ 1) $ streamRepeat 2 == 3333333333
 streamMap :: (a -> b) -> Stream a -> Stream b
 streamMap f (Cons a b) = Cons (f a) $ streamMap f b
+
+-- streamFromSeed (+1) 0 == 1234567891011
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f s =
+  let seed = f s
+  in Cons seed $ streamFromSeed f seed
+        
