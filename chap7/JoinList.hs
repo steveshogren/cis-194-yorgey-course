b23590634fcbcac12e203990da409b48563cc29d
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-17 14:39:13 -0400

    Finishing the dropJ that removes elements from the tree with O(log n)

--- chap7/JoinList.hs
+++ chap7/JoinList.hs
@@ -18,6 +18,7 @@ a = Single (Size 1) "a"
 b = Single (Size 1) "b"
 c = Single (Size 1) "c"
 d = Single (Size 1) "d"
+jl = (a +++ b) +++ (c +++ d)
 
 -- indexJ 0 $ (a +++ b) +++ (c +++ d) == Just "a"
 -- indexJ 1 $ (a +++ b) +++ (c +++ d) == Just "b"
@@ -54,21 +55,21 @@ getCount Empty = 0
 getCount (Single cnt _) =  getSize(size(cnt))
 getCount (Append cnt _ _) =  getSize(size(cnt))
 
---           6
---      3         3
---   1     2    1   2
---        1 1      1 1
+-- foldr (\n ac -> ac && jlToList (dropJ n jl) == drop n (jlToList jl)) True [0..4]
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ 0 jl = jl 
 dropJ i (Single _ _) | i > 0  = Empty
 dropJ i n@(Append cnt l r) =
   let leftCnt = getCount l 
       rightCnt = getCount r
-      dropLeft = leftCnt <= i
-      dropWholeList = i >= getCount n
-      out = "" -- " | i:" ++ show i ++ " count: " ++ show(getCount cnt) ++ " leftc: " ++ show leftCnt ++ " rightc: " ++ show rightCnt
+      dropWholeLeft = leftCnt <= i
+      dropEverything = i >= getCount n
   in case () of
-    _ | dropWholeList -> trace ("Drop Whole" ++ out) Empty
-      | dropLeft      -> trace ("Drop to right" ++ out) $ dropJ (i - leftCnt) r
-      | otherwise     -> trace ("Drop to left" ++ out) $ dropJ i l
+    _ | dropEverything -> Empty
+      | dropWholeLeft  -> dropJ (i - leftCnt) r
+      | otherwise      -> 
+          let newLeft = dropJ i l
+              newLeftCount = tag newLeft
+              newCount = newLeftCount <> (tag r)
+          in Append newCount newLeft r
 
