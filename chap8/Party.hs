c93c20424c27b8ee7ac1368dd5b5cac74f5cf96c
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-18 07:04:41 -0400

    A function to return the STFUer of two GuestLists

--- chap8/Party.hs
+++ chap8/Party.hs
@@ -1,5 +1,12 @@
 -- module Party
 import Employee
 
+gl = GL [] 0
+stan = (Emp "Stan" 4)
+
 glCons :: Employee -> GuestList -> GuestList
 glCons em (GL emps funs) = GL (em : emps) $ funs + empFun em 
+
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun g1@(GL _ f1) g2@(GL _ f2) = if f1 > f2 then g1 else g2
