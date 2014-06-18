1ddf2c0326c0ec2846ee9151ef9a65ce93b18884
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-18 07:14:10 -0400

    The monoid instance for GuestList

--- chap8/Party.hs
+++ chap8/Party.hs
@@ -1,12 +1,21 @@
--- module Party
+module Party where
+
 import Employee
+import Data.Monoid
 
 gl = GL [] 0
 stan = (Emp "Stan" 4)
+dave = (Emp "Dave" 1)
+gl1 = glCons stan gl
+gl2 = glCons dave gl
 
 glCons :: Employee -> GuestList -> GuestList
 glCons em (GL emps funs) = GL (em : emps) $ funs + empFun em 
 
-
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun g1@(GL _ f1) g2@(GL _ f2) = if f1 > f2 then g1 else g2
+
+instance Monoid GuestList where
+  mempty  = GL [] 0
+  mappend g1@(GL es1 f1) g2@(GL es2 f2) = GL (es1 ++ es2) $ f1 + f2
+
