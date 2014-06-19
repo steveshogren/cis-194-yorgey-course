79d612b9925b423b978ae9be5b47dd7e0aebd508
Steve Shogren <steve.a.shogren@gmail.com>
2014-06-19 07:14:04 -0400

    WIP for a Tree fold function

--- chap8/Party.hs
+++ chap8/Party.hs
@@ -2,12 +2,15 @@ module Party where
 
 import Employee
 import Data.Monoid
+import Data.Tree
 
 gl = GL [] 0
 stan = (Emp "Stan" 4)
 dave = (Emp "Dave" 1)
 gl1 = glCons stan gl
 gl2 = glCons dave gl
+
+x = Node {rootLabel="Test", subForest=[] }
 
 glCons :: Employee -> GuestList -> GuestList
 glCons em (GL emps funs) = GL (em : emps) $ funs + empFun em 
@@ -19,3 +22,5 @@ instance Monoid GuestList where
   mempty  = GL [] 0
   mappend g1@(GL es1 f1) g2@(GL es2 f2) = GL (es1 ++ es2) $ f1 + f2
 
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold f acum (Node{rootLabel=l,subForest=[]}) = f l acum    
