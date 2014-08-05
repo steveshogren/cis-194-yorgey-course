5e2a196a1f86a712e6303b43bead3af8f9cc92e3
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-05 07:05:47 -0400

    Getting a way to list all directories under a certain one

--- dateParser.hs
+++ dateParser.hs
@@ -4,17 +4,29 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import System.Directory
 import Data.Char
+import Control.Monad
 import qualified Data.Text as T
 
-codeDirectory = "/home/jack/programing"
+-- codeDirectory = "/home/jack/programing"
 
-searchPath = codeDirectory ++ "/**/.git"
+-- searchPath = codeDirectory ++ "/**/.git"
+
+listDirs :: [FilePath] -> IO [FilePath]
+listDirs = filterM doesDirectoryExist
+
+joinFN :: String -> String -> FilePath
+joinFN p1 p2 = joinPath [p1, p2]
+
+-- isDoDD f = not $ (endsWith "/." f) || (endsWith "/.." f)
+
 
 
 main = do
---  all <- getDirectoryContents "/home/jack/programming"
-  all <- getDirectoryContents "/home/jack/programming/sicp"
-  let filtered = filter (isPrefixOf ".git") all
-  print filtered
+  let path =  "/home/jack/programming/"
+  all <- getDirectoryContents path
+  --noDots <- filterM (return . isDoDD) (map (joinFN path) all)
+  dirs <- listDirs $ map (joinFN path) all
+  let filtered = filter (isPrefixOf ".git") dirs
+  print dirs
 
 
