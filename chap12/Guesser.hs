b080704efe7d00ce7f43ed1d22738d23b2f46b93
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-22 07:15:31 -0400

    Making the identifier for the ofAKind hands

--- chap12/Guesser.hs
+++ chap12/Guesser.hs
@@ -5,6 +5,8 @@ data Suit = Clubs | Spades | Hearts | Diamonds
 
 type FaceValue = Int
 
+data Hand = TwoKind FaceValue | ThreeKind FaceValue | FourKind FaceValue | HighCard
+  deriving (Eq, Ord, Show)
 
 data Card = Card { fv :: FaceValue, s :: Suit } 
   deriving (Eq, Show, Ord)
@@ -19,12 +21,23 @@ parse str = Card  (read $ tail str)  (parseSuit $ head str)
 
 parseHand = map parse . words 
 
+
+grouped = groupBy (\c1 c2 -> (==) (fv c1) (fv c2) ) . sort 
+
+ofAKind num h = filter (\x -> length x > (num - 1)) $ grouped h
+
+identify x =
+  if length (ofAKind 4 x) > 0 then
+    FourKind 1
+  else if length (ofAKind 3 x) > 0 then
+    ThreeKind 1
+  else if length (ofAKind 2 x) > 0 then
+    TwoKind 1
+  else HighCard
+  
 h1 = parseHand "H6 H2 D2 D8 C9"
-
--- identify = groupBy (\c1 c2 -> (==) (fv c1) (fv c2) ) . sort 
-identify = groupBy (\c1 c2 -> (== fv c1) $ fv c2 ) . sort 
-
-g1 = identify h1
+h2 = parseHand "H2 H2 D2 D8 C9"
+h3 = parseHand "H2 H2 D2 D2 C9"
 
 
 
