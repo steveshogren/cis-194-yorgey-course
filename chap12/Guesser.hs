520f00c51a65db597e748025da7c0a0c2fe70632
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-23 07:14:38 -0400

    Adding flush detection

--- chap12/Guesser.hs
+++ chap12/Guesser.hs
@@ -5,7 +5,11 @@ data Suit = Clubs | Spades | Hearts | Diamonds
 
 type FaceValue = Int
 
-data Hand = TwoKind FaceValue | ThreeKind FaceValue | FourKind FaceValue | HighCard
+data Hand = TwoKind FaceValue
+          | ThreeKind FaceValue
+          | FourKind FaceValue
+          | HighCard
+          | Flush Suit
   deriving (Eq, Ord, Show)
 
 data Card = Card { fv :: FaceValue, s :: Suit } 
@@ -21,32 +25,40 @@ parse str = Card  (read $ tail str)  (parseSuit $ head str)
 
 parseHand = map parse . words 
 
+groupFn f = groupBy (\c1 c2 -> (==) (f c1) (f c2))  
 
-grouped = groupBy (\c1 c2 -> (==) (fv c1) (fv c2) ) . sort 
+grouped = groupFn fv . sort 
 
-ofAKind num h = filter (\x -> length x > (num - 1)) $ grouped h
 -- ofAKind 2 h1
+ofAKind num h = filter (\x -> length x > (num - 1)) $ grouped h
+
+flush x =
+  let g = groupFn s x
+  in (length (head g) == 5, g)
+
 identify x =
   let t4 = ofAKind 4 x
       t3 = ofAKind 3 x
       t2 = ofAKind 2 x
-  in if length t4 > 0 then
-    FourKind (fv $ head $ head t4)
-  else if length t3 > 0 then
-    ThreeKind (fv $ head $ head t3)
-  else if length t2 > 0 then
-    TwoKind (fv $ head $ head t2)
-  else HighCard
+      (isFlush, f1) = flush x
+  in if isFlush then
+       Flush $ s . head . head $ f1
+     else if length t4 > 0 then
+            FourKind (fv $ head $ head t4)
+          else if length t3 > 0 then
+                 ThreeKind (fv $ head $ head t3)
+               else if length t2 > 0 then
+                      TwoKind (fv $ head $ head t2)
+                    else HighCard
   
 h1 = parseHand "H6 H2 D2 D8 C9"
 h2 = parseHand "H2 H2 D2 D8 C9"
 h3 = parseHand "H2 H2 D2 D2 C9"
+h4 = parseHand "H2 H5 H7 H10 H9"
 
 rt = 
-    let i1 = identify h1
-        i2 = identify h2
-        i3 = identify h3
-    in i1 == TwoKind 2 &&
-       i2 == ThreeKind 2 &&
-       i3 == FourKind 2
+    identify h1 == TwoKind 2 &&
+    identify h2 == ThreeKind 2 &&
+    identify h3 == FourKind 2 &&
+    identify h4 == Flush Hearts
 
