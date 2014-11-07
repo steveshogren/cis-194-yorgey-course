a09f24ff9e95218808250b26360d68fd21c482db
Steve Shogren <steve.a.shogren@gmail.com>
2014-11-07 16:29:48 -0500

    Refactoring the tests to gather all failures

--- chap12/Guesser.hs
+++ chap12/Guesser.hs
@@ -133,20 +133,25 @@ winnerRunner hands expected =
    (actual, Left expected) -> actual == expected
    _ -> False
 
-ast actual expected =
-  (identify (parseHand actual)) == (Left expected)
+ast :: String -> Hand -> [String] -> [String]
+ast actual expected state =
+  let actu = (identify (parseHand actual))
+      result = actu == (Left expected)
+  in if result
+     then state
+     else ("FAILED  Actual: " ++ (show actu) ++ " -> Expected: " ++ (show expected)) : state 
   
 rt =
-    ast "H6 H2 D2 D8 C9" (TwoKind 2)
-    && ast "H2 H2 D2 D8 C9" (ThreeKind 2)
-    && ast "H2 H2 D2 D2 C9" (FourKind 2)
-    && ast "H2 H5 H7 H10 H9" (Flush Hearts)
-    && ast "H2 H2 D2 D9 C9" (FullHouse 2 9)  -- three 2s higher
-    && ast "H6 S3 H4 D5 H2" (Straight 6) 
-    && ast "HA S3 H4 D5 H2" (Straight 5)
-    && ast "HA S10 H13 D12 H11" (Straight 14) 
-    && ast "H6 H3 H4 H5 H2" (StraightFlush 6 Hearts)
-    && winnerRunner [k3, k2] k3
-    && winnerRunner [k3, fl, k2] fl
-    && winner [TwoKind 3, TwoKind 10] == TwoKind 10
+    ast "H6 H2 D2 D8 C9" (TwoKind 2) []
+    -- && ast "H2 H2 D2 D8 C9" (ThreeKind 2)
+    -- && ast "H2 H2 D2 D2 C9" (FourKind 2)
+    -- && ast "H2 H5 H7 H10 H9" (Flush Hearts)
+    -- && ast "H2 H2 D2 D9 C9" (FullHouse 2 9)  -- three 2s higher
+    -- && ast "H6 S3 H4 D5 H2" (Straight 6) 
+    -- && ast "HA S3 H4 D5 H2" (Straight 5)
+    -- && ast "HA S10 H13 D12 H11" (Straight 14) 
+    -- && ast "H6 H3 H4 H5 H2" (StraightFlush 6 Hearts)
+    -- && winnerRunner [k3, k2] k3
+    -- && winnerRunner [k3, fl, k2] fl
+    -- && winner [TwoKind 3, TwoKind 10] == TwoKind 10
 
