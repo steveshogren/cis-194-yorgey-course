53755b5965aeffdd05dbabdf39ff8a3598090df3
Steve Shogren <steve.a.shogren@gmail.com>
2014-11-13 19:29:33 -0500

    Cleaning up the winner tests to not use Either

--- chap12/GuesserTests.hs
+++ chap12/GuesserTests.hs
@@ -4,16 +4,6 @@ import Data.Either
 import Control.Monad
 import Guesser
 
-k2 = identify $ parseHand "H6 H2 D2 D8 C9"
-k3 = identify $ parseHand "H2 H2 D2 D8 C9"
-k4 = identify $ parseHand "H2 H2 D2 D2 C9"
-fl = identify $ parseHand "H2 H5 H7 H10 H9"
-
-winnerRunner hands expected =
-  case (winner (lefts hands), expected) of
-   (actual, Left expected) -> actual == expected
-   _ -> False
-
 ast :: (String, Hand) -> [String] -> [String]
 ast (actual, expected) state =
   let actu = (identify (parseHand actual))
@@ -35,6 +25,11 @@ rt =
                 ("HA S10 H13 D12 H11", (Straight 14)),
                 ("H6 H3 H4 H5 H2", (StraightFlush 6 Hearts))]
 
-rot = winnerRunner [k3, k2] k3
-      && winnerRunner [k3, fl, k2] fl
+k2 = TwoKind 2
+k3 = ThreeKind 2
+k4 = FourKind 2
+fl = Flush Hearts
+
+rot = winner [k3, k2] == k3
+      && winner [k3, fl, k2] == fl
       && winner [TwoKind 3, TwoKind 10] == TwoKind 10
