e5afa484f3c5e94427e300415c2117a7fa894b33
Steve Shogren <steve.a.shogren@gmail.com>
2015-04-15 09:54:57 -0400

    Replacing a homemade solution to walk looking for .git dirs with filemanip

--- dateParser.hs
+++ dateParser.hs
@@ -1,32 +1,11 @@
 import System.FilePath
-import Data.List
-import Data.Map (Map)
-import qualified Data.Map as Map
 import System.Directory
-import Data.Char
+import Data.String.Utils
 import Control.Monad
-import qualified Data.Text as T
+import System.FilePath.Find
 
--- codeDirectory = "/home/jack/programing"
-
--- searchPath = codeDirectory ++ "/**/.git"
-
-listDirs :: [FilePath] -> IO [FilePath]
-listDirs = filterM doesDirectoryExist
-
-joinFN :: String -> String -> FilePath
-joinFN p1 p2 = joinPath [p1, p2]
-
--- isDoDD f = not $ (endsWith "/." f) || (endsWith "/.." f)
-
-
-
+main :: IO()
 main = do
   let path =  "/home/jack/programming/"
-  all <- getDirectoryContents path
-  --noDots <- filterM (return . isDoDD) (map (joinFN path) all)
-  dirs <- listDirs $ map (joinFN path) all
-  let filtered = filter (isPrefixOf ".git") dirs
-  print dirs
-
-
+  onlyGits <- find always (fileType ==? Directory &&? fileName ==? ".git") path
+  print onlyGits
