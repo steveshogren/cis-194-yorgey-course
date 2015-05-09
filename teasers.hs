f5d5c1bca70908d2a06f73834beb8fff4efc5da3
Steve Shogren <steve.a.shogren@gmail.com>
2015-05-09 11:48:39 -0400

    Finishing the list sort for largest number

--- teasers.hs
+++ teasers.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
-import Data.List(groupBy, sortBy)
+import Data.List(groupBy, sortBy, sort)
 import Data.Function(on)
 
 combine :: [a] -> [a] -> [a]
@@ -16,13 +16,32 @@ iFib n prev next acc =
 
 nthFib n = reverse $ iFib n 0 1 [0]
 
+firstDigit :: Show a => a -> Char
 firstDigit = head . show
+
+firstDigitOfList :: Show b => [b] -> Char
 firstDigitOfList = firstDigit . head
 
-sortLargestTotal n =
-  reverse $
-  sortBy (compare `on` firstDigitOfList) $
-         groupBy ((==) `on` firstDigit) n
+padR :: Char -> Int -> String -> String
+padR with n s
+    | length s < n  = s ++ replicate (n - length s) with
+    | otherwise     = s
+
+widestNum :: (Ord b, Show b) => [b] -> Int
+widestNum  =  length . show . maximum
+
+sortGrouping :: (Ord a, Show a) => [a] -> [a]
+sortGrouping g =
+  let largestNum = widestNum g
+      leadChar = head . show . head $ g
+  in sortBy (flip compare `on` padR leadChar largestNum . show) g
+
+sortLargestTotal :: (Ord a, Show a) => [a] -> [[a]]
+sortLargestTotal =
+  map sortGrouping .
+  sortBy (flip compare `on` firstDigitOfList) .
+  groupBy ((==) `on` firstDigit) .
+  sortBy (compare `on` firstDigit)
 
 main :: IO ()
-main = putStrLn "test"
+main = print  $ sortLargestTotal [50, 9, 95, 29, 55, 5, 59, 51, 2, 22]
