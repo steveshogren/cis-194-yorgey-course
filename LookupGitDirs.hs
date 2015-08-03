d1840521ff1cc27fcd64aafe8c2b3f300dc518a5
Steve Shogren <steve.a.shogren@gmail.com>
2015-08-03 07:59:18 -0400

    Adding an ignores file to Glob against

--- LookupGitDirs.hs
+++ LookupGitDirs.hs
@@ -1,19 +1,20 @@
 module LookupGitDirs (getGitDirectories) where
 
 import System.FilePath.Find (FindClause, find, fileName, depth, (<=?), (==?), (/~?), (&&?), filePath)
+import Control.Monad
 
-filterIgnored :: [String]
-filterIgnored = ["convincing-coworkers"]
+getIgnoredList :: IO [String]
+getIgnoredList = liftM words $ readFile "/home/jack/programming/haskell-course/gitignores"
 
-addIgnored :: FindClause Bool -> FindClause Bool
-addIgnored appendTo = foldl (\res next -> res &&? (filePath /~? "**" ++ next ++"**"))
-                      appendTo filterIgnored
+addIgnored :: FindClause Bool -> [String] -> FindClause Bool
+addIgnored = foldl (\res next -> res &&? (filePath /~? "**" ++ next ++"**"))
 
-filterFor :: System.FilePath.Find.FindClause Bool
+filterFor :: [String] -> FindClause Bool
 filterFor = addIgnored (fileName ==? ".git") 
 
 getGitDirectories :: IO [FilePath]
 getGitDirectories = do
   let path =  "/home/jack/programming/"
-  onlyGits <- find (depth <=? 3) filterFor path
+  ignores <- getIgnoredList
+  onlyGits <- find (depth <=? 3) (filterFor ignores) path
   return $ onlyGits ++ ["/home/jack/.emacs.bak/.git", "/home/jack/private/.git"]
