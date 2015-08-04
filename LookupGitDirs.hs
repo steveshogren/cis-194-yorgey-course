960190ddc411255a903efee539597fc1b108b647
Steve Shogren <steve.a.shogren@gmail.com>
2015-08-04 07:53:54 -0400

    Adding a 'find missing in current' helper

--- LookupGitDirs.hs
+++ LookupGitDirs.hs
@@ -2,6 +2,9 @@ module LookupGitDirs (getGitDirectories) where
 
 import System.FilePath.Find (FindClause, find, fileName, depth, (<=?), (==?), (/~?), (&&?), filePath)
 import Control.Monad
+import Data.Maybe (catMaybes)
+import System.Process (readProcessWithExitCode)
+import System.Exit (ExitCode(..))
 
 getIgnoredList :: IO [String]
 getIgnoredList = liftM words $ readFile "/home/jack/programming/haskell-course/gitignores"
@@ -18,3 +21,14 @@ getGitDirectories = do
   ignores <- getIgnoredList
   onlyGits <- find (depth <=? 3) (filterFor ignores) path
   return $ onlyGits ++ ["/home/jack/.emacs.bak/.git", "/home/jack/private/.git"]
+
+listGitDirsWithCurrentChanges repoPath = do
+  (code,stdout,_) <- readProcessWithExitCode "git" ["--git-dir", repoPath, "rev-list", "current", "^master", "--no-merges"] "."
+  return $ if code == ExitSuccess then Just (repoPath,words stdout) else Nothing
+
+getGits = do
+  gits <- getGitDirectories
+  dires <- mapM listGitDirsWithCurrentChanges gits
+  return $ catMaybes dires
+
+-- "git rev-parse --verify current && git lg current ^master --no-merges"
